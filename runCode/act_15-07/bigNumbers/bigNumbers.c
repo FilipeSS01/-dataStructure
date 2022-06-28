3286e3e82a414a091c04b6ac0b4bbe90a68f35d4
Filipe Silva <filipess671@gmail.com>
2022-06-28 20:05:38 -0300

    Update dataStructure - Update insert bigNumbers

--- runCode/act_15-07/bigNumbers/bigNumbers.c
+++ runCode/act_15-07/bigNumbers/bigNumbers.c
@@ -17,16 +17,21 @@ typedef struct List
 } List;
 
 void initialize(List *list);
-int insert(List *list, int value);
+int insert(List *list);
 int print(List list);
 
 int main()
 {
     List numbers1;
     List numbers2;
-
     initialize(&numbers1);
     initialize(&numbers2);
+
+    insert(&numbers1);
+    insert(&numbers2);
+
+    print(numbers1);
+    print(numbers2);
     return 0;
 }
 
@@ -35,27 +40,34 @@ void initialize(List *list)
     list->start = NULL;
     list->end = NULL;
 }
-int insert(List *list, int value)
+int insert(List *list)
 {
-    Node *aux = malloc(sizeof(Node));
-    if (aux == NULL)
-        return -1;
-
-    aux->value = value;
-    if (list->start == NULL)
+    int value = 0;
+    do
     {
-        list->start = aux;
-        list->end = aux;
-        aux->next = NULL;
-        aux->prev = NULL;
-    }
-    else
-    {
-        list->end->next = aux;
-        aux->prev = list->end;
-        aux->next = NULL;
-        list->end = aux;
-    }
+        scanf("%d", &value);
+        if (value != -1)
+        {
+            Node *aux = malloc(sizeof(Node));
+            if (aux == NULL)
+                return -1;
+            aux->value = value;
+            if (list->start == NULL)
+            {
+                list->start = aux;
+                list->end = aux;
+                aux->next = NULL;
+                aux->prev = NULL;
+            }
+            else
+            {
+                list->end->next = aux;
+                aux->prev = list->end;
+                aux->next = NULL;
+                list->end = aux;
+            }
+        }
+    } while (value != -1);
     return 0;
 }
 int print(List list)
