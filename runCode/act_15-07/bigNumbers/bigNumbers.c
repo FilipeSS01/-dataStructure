e1c5e0cf4731d6427cfb4072a637253a2d737a6a
Filipe Silva <filipess671@gmail.com>
2022-06-27 23:30:04 -0300

    Update dataStructure - Starting code bigNumbers

---
+++ runCode/act_15-07/bigNumbers/bigNumbers.c
@@ -0,0 +1,21 @@
+// Develop by Filipe Silva
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int dado;
+    struct Node *next;
+} Node;
+
+typedef struct List
+{
+    Node *start;
+    Node *end;
+} List;
+
+int main()
+{
+    return 0;
+}
