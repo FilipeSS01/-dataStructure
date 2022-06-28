cdca04cfd0fce84a197fd84cf054d40c4546a2c0
Filipe Silva <filipess671@gmail.com>
2022-06-27 23:33:13 -0300

    Update dataStructure - Added initialize bigNumbers

--- runCode/act_15-07/bigNumbers/bigNumbers.c
+++ runCode/act_15-07/bigNumbers/bigNumbers.c
@@ -15,7 +15,21 @@ typedef struct List
     Node *end;
 } List;
 
+void initialize(List *list);
+int insert(List *list, int dado);
+
 int main()
 {
+    List *numbers1;
+    List *numbers2;
+
+    initialize(&numbers1);
+    initialize(&numbers2);
     return 0;
+}
+
+void initialize(List *list)
+{
+    list->start = NULL;
+    list->end = NULL;
 }
