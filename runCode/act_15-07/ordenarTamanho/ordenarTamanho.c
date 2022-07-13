32cb928ac900407dbbbf16fbeea3df9ed32b4286
Filipe Silva <filipess671@gmail.com>
2022-07-13 14:51:00 -0300

    dataStructure - Finished splitMatrix() ordenarTamanho

--- runCode/act_15-07/ordenarTamanho/ordenarTamanho.c
+++ runCode/act_15-07/ordenarTamanho/ordenarTamanho.c
@@ -2,9 +2,41 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+#define MAX_STR 500
+
+typedef struct words
+{
+    char str[MAX_STR];
+} Words;
+
+int splitMatrix(char *string, Words *words);
 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+    Words *words = (Words *)malloc(50 * sizeof(Words));
+    char str[MAX_STR];
+    int size = 0;
+    fgets(str, sizeof(str), stdin);
+    size = splitMatrix(str, words);
 
     return 0;
+}
+
+int splitMatrix(char *string, Words *words)
+{
+    int i = 0;
+
+    char *token = strtok(string, " ");
+    while (token != NULL)
+    {
+        strcpy(words[i].str, token);
+        token = strtok(NULL, " ");
+        i++;
+    }
+
+    return i;
 }
