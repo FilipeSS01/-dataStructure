f30ab1c72d04ee71d8aeb99db1aa8df60a04c8e5
Filipe Silva <filipess671@gmail.com>
2022-07-15 11:39:34 -0300

    dataStructure - Added push() and pop() in posFixada.c

--- runCode/act_15-07/posFixada/posFixada.c
+++ runCode/act_15-07/posFixada/posFixada.c
@@ -3,8 +3,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct stack
+{
+    float dado;
+    struct stack *next;
+} Stack;
+
+void initialize(Stack *stack);
+int push(Stack *stack, float dado);
+int pop(Stack *stack);
+
 int main()
 {
 
     return 0;
+}
+
+void initialize(Stack *stack)
+{
+    // stack->dado = NULL;
+    stack->next = NULL;
+}
+int push(Stack *stack, float dado)
+{
+    Stack *new = (Stack *)malloc(sizeof(Stack));
+
+    if (new == NULL)
+        return 0;
+
+    new->dado = dado;
+    new->next = stack->next;
+    stack->next = new;
+
+    return 1;
 }
